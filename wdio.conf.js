5ec3d9f30a3a7be084d7fea3c4311ea7d98da0ef
Nickkarwag1 <nick98_molo@icloud.com>
2021-12-20 22:12:13 +0300

    rename file onlynerByTest

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './test/OnlynerByTest.js'
+        './test/onlynerByTest.js'
     ],
     // Patterns to exclude.
     exclude: [
