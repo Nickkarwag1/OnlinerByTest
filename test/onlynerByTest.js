ae5caceb29dff1ccfcf137ebe1b3189c54852288
Nickkarwag1 <nick98_molo@icloud.com>
2021-12-21 12:43:11 +0300

    Open browser page in url and input login & password

--- test/onlynerByTest.js
+++ test/onlynerByTest.js
@@ -1,7 +1,16 @@
 describe('Open Page', function() {
-    it('Open browser page in url', async function () {
+        it('Open browser page in url and input login & password', async function () {
         await browser.url('https://www.onliner.by/');
-        console.log(await browser.getUrl());
-        await browser.pause(15000)
+        await browser.pause(5000)
+        const enterButton = $('//div[text()=\'Вход\']');
+        await browser.pause(3000);
+        enterButton.click();
+        await browser.pause(3000);
+        let inputMail = $('//input[@placeholder = \'Ник или e-mail\']');
+        inputMail.addValue('test@gmail.com');
+        await browser.pause(3000);
+        let inputPassword = $('//input[@placeholder = \'Пароль\']');
+        inputPassword.addValue('12314351325')
+        await browser.pause(5000);
     });
 });
