9b13a6804992925cc6bda52fbc2f53573e1150e0
Nickkarwag1 <nick98_molo@icloud.com>
2021-12-21 16:34:25 +0300

    Added constants and prettier

--- test/onlynerByTest.js
+++ test/onlynerByTest.js
@@ -1,17 +1,21 @@
-describe('Open Page', function() {
-        it('Open browser page in url and input login & password', async function () {
-        await browser.url('https://www.onliner.by/');
-        await browser.pause(5000)
-        const enterButton = $('//div[text()=\'Вход\']');
-        await browser.pause(3000);
-        enterButton.click();
-        await browser.pause(3000);
-        const user = {username: 'test@gmail.com', password:'12314351325'}
-        const inputMail = $('//input[@placeholder = \'Ник или e-mail\']');
-        inputMail.addValue(user.username);
-        await browser.pause(3000);
-        const inputPassword = $('//input[@placeholder = \'Пароль\']');
-        inputPassword.addValue(user.password)
-        await browser.pause(5000);
-    });
+const USER = { USERNAME: "test@gmail.com", PASSWORD: "12314351325" };
+const EMAIL_INPUT_SELECTOR = "//input[@placeholder = 'Ник или e-mail']";
+const PASSWORD_INPUT_SELECTOR = "//input[@placeholder = 'Пароль']";
+const ENTER_BUTTON_SELECTOR = "//div[text()='Вход']";
+
+describe("Open Page", function () {
+  it("Open browser page in url and input login & password", async function () {
+    await browser.url("https://www.onliner.by/");
+    await browser.pause(5000);
+    const enterButton = $(ENTER_BUTTON_SELECTOR);
+    await browser.pause(3000);
+    enterButton.click();
+    await browser.pause(3000);
+    const inputMail = $(EMAIL_INPUT_SELECTOR);
+    inputMail.addValue(USER.USERNAME);
+    await browser.pause(3000);
+    const inputPassword = $(PASSWORD_INPUT_SELECTOR);
+    inputPassword.addValue(USER.PASSWORD);
+    await browser.pause(5000);
+  });
 });
