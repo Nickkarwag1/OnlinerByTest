b082148917b304c546bd33187b0a1ec16ed10c17
Nickkarwag1 <nick98_molo@icloud.com>
2022-01-06 18:47:07 +0300

    Dubbing fixes

--- test/testAPI.e2e.js
+++ test/testAPI.e2e.js
@@ -1,34 +1,47 @@
 const request = require("supertest");
 const {expect} = require("chai");
 
-const URL = `https://reqres.in`
+const BASE_URL = `https://reqres.in`
+
+let response;
+
+const URL = {
+    LIST_USER: `/api/users?page=2`,
+    SINGLE_USER: `/api/users/2`,
+    USER_NOT_FOUND: `/api/users/23`,
+    LIST_RESOURCE: `/api/unknown`,
+    CREAT_USER: `/api/users`,
+    UPDATE_AND_DELETE_USER: `/api/users/2`,
+    REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER: `/api/register`,
+    LOGIN_USER: `/api/login`,
+}
 
 describe("test API", function () {
     it("GET list user", async function () {
-        const response = await request(URL).get(`/api/users?page=2`);
+        response = await request(BASE_URL).get(URL.LIST_USER);
         expect(response.statusCode).to.eql(200);
         expect(response.body).to.have.property("page");
     });
 
     it("GET single user", async function () {
-        const response = await request(URL).get(`/api/users/2`);
+        response = await request(BASE_URL).get(URL.SINGLE_USER);
         expect(response.statusCode).to.eql(200)
     });
 
     it("GET single user not found", async function () {
-        const response = await request(URL).get(`/api/users/23`);
+        response = await request(BASE_URL).get(URL.USER_NOT_FOUND);
         expect(response.statusCode).to.eql(404)
     });
 
     it("GET list resource", async function () {
-        const response = await request(URL).get(`/api/unknown`);
+        response = await request(BASE_URL).get(URL.LIST_RESOURCE);
         expect(response.statusCode).to.eql(200);
         expect(response.body).to.have.property("data");
         expect(response.body.data[2]).to.have.property("id");
     });
 
     it("POST creat", async function () {
-        const response = await request(URL).post(`/api/users`).send({
+        response = await request(BASE_URL).post(URL.CREAT_USER).send({
             name: "morpheus",
             job: "leader",
         });
@@ -37,7 +50,7 @@ describe("test API", function () {
     });
 
     it("PUT creat", async function () {
-        const response = await request(URL).put(`/api/users/2`).send({
+        response = await request(BASE_URL).put(URL.UPDATE_AND_DELETE_USER).send({
             name: "morpheus",
             job: "zion resident",
         });
@@ -46,12 +59,12 @@ describe("test API", function () {
     });
 
     it("DELETE", async function () {
-        const response = await request(URL).delete(`/api/users/2`);
+        response = await request(BASE_URL).delete(URL.UPDATE_AND_DELETE_USER);
         expect(response.statusCode).to.eql(204)
     });
 
     it("POST register - successful", async function () {
-        const response = await request(URL).post(`/api/register`).send({
+        response = await request(BASE_URL).post(URL.REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER).send({
             email: "eve.holt@reqres.in",
             password: "pistol",
         });
@@ -62,7 +75,7 @@ describe("test API", function () {
     });
 
     it("POST register - unsuccessful", async function () {
-        const response = await request(URL).post(`/api/register`).send({
+        response = await request(BASE_URL).post(URL.REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER).send({
             email: "sydney@fife",
         });
         expect(response.statusCode).to.eql(400);
@@ -70,7 +83,7 @@ describe("test API", function () {
     });
 
     it("POST login successful", async function () {
-        const response = await request(URL).post(`/api/login`).send({
+        response = await request(BASE_URL).post(URL.LOGIN_USER).send({
             email: "eve.holt@reqres.in",
             password: "cityslicka",
         });
