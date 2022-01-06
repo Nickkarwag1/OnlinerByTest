a12e4359cdbeebe2c80263ea3779eff4538eae89
Nickkarwag1 <nick98_molo@icloud.com>
2022-01-06 19:39:48 +0300

    Dubbing fixes 2

--- test/testAPI.e2e.js
+++ test/testAPI.e2e.js
@@ -16,78 +16,97 @@ const URL = {
     LOGIN_USER: `/api/login`,
 }
 
+const DATA = {
+    PAGE: "page",
+    DATA: "data",
+    ID: "id",
+    UPDATED_AT: "updatedAt",
+    ERROR: "error",
+    TOKEN: "token",
+}
+
+const STATUS_CODE = {
+    OK: 200,
+    CREATED: 201,
+    NO_CONTENT: 204,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+}
+
+const app =  request(BASE_URL)
+
 describe("test API", function () {
     it("GET list user", async function () {
-        response = await request(BASE_URL).get(URL.LIST_USER);
-        expect(response.statusCode).to.eql(200);
-        expect(response.body).to.have.property("page");
+        response = await app.get(URL.LIST_USER);
+        expect(response.statusCode).to.eql(STATUS_CODE.OK);
+        expect(response.body).to.have.property(DATA.PAGE);
     });
 
     it("GET single user", async function () {
-        response = await request(BASE_URL).get(URL.SINGLE_USER);
-        expect(response.statusCode).to.eql(200)
+        response = await app.get(URL.SINGLE_USER);
+        expect(response.statusCode).to.eql(STATUS_CODE.OK)
     });
 
     it("GET single user not found", async function () {
-        response = await request(BASE_URL).get(URL.USER_NOT_FOUND);
-        expect(response.statusCode).to.eql(404)
+        response = await app.get(URL.USER_NOT_FOUND);
+        expect(response.statusCode).to.eql(STATUS_CODE.NOT_FOUND)
     });
 
     it("GET list resource", async function () {
-        response = await request(BASE_URL).get(URL.LIST_RESOURCE);
-        expect(response.statusCode).to.eql(200);
-        expect(response.body).to.have.property("data");
-        expect(response.body.data[2]).to.have.property("id");
+        response = await app.get(URL.LIST_RESOURCE);
+        expect(response.statusCode).to.eql(STATUS_CODE.OK);
+        expect(response.body).to.have.property(DATA.DATA);
+        expect(response.body.data[2]).to.have.property(DATA.ID);
     });
 
     it("POST creat", async function () {
-        response = await request(BASE_URL).post(URL.CREAT_USER).send({
+        response = await app.post(URL.CREAT_USER).send({
             name: "morpheus",
             job: "leader",
         });
-        expect(response.statusCode).to.eql(201)
-        expect(response.body).to.have.property("id");
+        expect(response.statusCode).to.eql(STATUS_CODE.CREATED)
+        expect(response.body).to.have.property(DATA.ID);
     });
 
     it("PUT creat", async function () {
-        response = await request(BASE_URL).put(URL.UPDATE_AND_DELETE_USER).send({
+        response = await app.put(URL.UPDATE_AND_DELETE_USER).send({
             name: "morpheus",
             job: "zion resident",
         });
-        expect(response.statusCode).to.eql(200);
-        expect(response.body).to.have.property("updatedAt");
+        expect(response.statusCode).to.eql(STATUS_CODE.OK);
+        expect(response.body).to.have.property(DATA.UPDATED_AT);
     });
 
     it("DELETE", async function () {
-        response = await request(BASE_URL).delete(URL.UPDATE_AND_DELETE_USER);
-        expect(response.statusCode).to.eql(204)
+        response = await app.delete(URL.UPDATE_AND_DELETE_USER);
+        expect(response.statusCode).to.eql(STATUS_CODE.NO_CONTENT)
     });
 
     it("POST register - successful", async function () {
-        response = await request(BASE_URL).post(URL.REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER).send({
+        response = await app.post(URL.REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER).send({
             email: "eve.holt@reqres.in",
             password: "pistol",
         });
-        expect(response.statusCode).to.eql(200);
-        expect(response.body).to.have.property("id");
-        expect(response.body).to.have.property("token");
+        expect(response.statusCode).to.eql(STATUS_CODE.OK);
+        expect(response.body).to.have.property(DATA.ID);
+        expect(response.body).to.have.property(DATA.TOKEN);
         expect(response.body.id).to.eql(4);
     });
 
     it("POST register - unsuccessful", async function () {
-        response = await request(BASE_URL).post(URL.REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER).send({
+        response = await app.post(URL.REGISTER_SUCCESSFUL_AND_UNSUCCESSFUL_USER).send({
             email: "sydney@fife",
         });
-        expect(response.statusCode).to.eql(400);
-        expect(response.body).to.have.property("error");
+        expect(response.statusCode).to.eql(STATUS_CODE.BAD_REQUEST);
+        expect(response.body).to.have.property(DATA.ERROR);
     });
 
     it("POST login successful", async function () {
-        response = await request(BASE_URL).post(URL.LOGIN_USER).send({
+        response = await app.post(URL.LOGIN_USER).send({
             email: "eve.holt@reqres.in",
             password: "cityslicka",
         });
-        expect(response.statusCode).to.eql(200);
-        expect(response.body).to.have.property("token");
+        expect(response.statusCode).to.eql(STATUS_CODE.OK);
+        expect(response.body).to.have.property(DATA.TOKEN);
     });
 });
